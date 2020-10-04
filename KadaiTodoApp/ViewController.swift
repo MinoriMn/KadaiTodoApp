2dbbd86bb5ad944002325cdf78899d663b773486
M_n <core.filemode>
2020-10-04 14:30:49 +0900

    update: binding storyboard and viewcontroller

--- KadaiTodoApp/ViewController.swift
+++ KadaiTodoApp/ViewController.swift
@@ -9,7 +9,7 @@ import UIKit
 import Combine
 
 class ViewController: UIViewController {
-    var label: UILabel!
+    @IBOutlet var label: UILabel!
     let viewModel = ViewModel()
     
     private var cancellables = Set<AnyCancellable>()
@@ -17,9 +17,9 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        label = UILabel()
-        label.frame = CGRect(x:100, y:200, width:160, height:30)
-        self.view.addSubview(label)
+//        label = UILabel()
+//        label.frame = CGRect(x:100, y:200, width:160, height:30)
+//        self.view.addSubview(label)
         
         viewModel.$labelText
             .map({ Optional($0) })
