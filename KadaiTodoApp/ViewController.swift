77a9dc6ddbf09da7c51c0360112a5ad464a40ed3
M_n <core.filemode>
2020-10-04 13:58:55 +0900

    add:ViewModel init

--- KadaiTodoApp/ViewController.swift
+++ KadaiTodoApp/ViewController.swift
@@ -6,12 +6,26 @@
 //
 
 import UIKit
+import Combine
 
 class ViewController: UIViewController {
+    var label: UILabel!
+    let viewModel = ViewModel()
+    
+    private var cancellables = Set<AnyCancellable>()
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        label = UILabel()
+        label.frame = CGRect(x:100, y:200, width:160, height:30)
+        self.view.addSubview(label)
+        
+        viewModel.$labelText
+            .map({ Optional($0) })
+            .receive(on: DispatchQueue.main)
+            .assign(to: \UILabel.text, on:label)
+            .store(in: &cancellables)
     }
 
 
---
+++ KadaiTodoApp/ViewModel.swift
@@ -0,0 +1,12 @@
+//
+//  ViewModel.swift
+//  KadaiTodoApp
+//
+//  Created by 松田尚也 on 2020/10/02.
+//
+
+import Combine
+
+class ViewModel {
+    @Published var labelText: String = "DEBUG"
+}
